bce8f43dd6c740d6995ad982dfbbf9248a6920de
Moe <belghith1@kenyon.edu>
2025-10-16 14:27:05 -0400

    submit

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -14,8 +14,26 @@ void LinkedList::push_front(int v) {
     head_ = new Node(v, head_);
 }
 void LinkedList::reverse() {
-    // TODO: implement
+    Node* p = head_;
+    LinkedList newList;  
+    while (p) 
+    {
+        newList.push_front(p->value);
+        p = p->next;
+    }
+
+    p = head_;
+    while (p) 
+    {
+        Node* n = p->next;
+        delete p;
+        p = n;
+    }
+    head_ = newList.head_;
+    newList.head_ = nullptr;
 }
+
+
 std::string LinkedList::to_string() const {
     std::ostringstream oss;
     oss << "[";
